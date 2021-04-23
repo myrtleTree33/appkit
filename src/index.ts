8ee45c56590cd8a9388cf040f84c4d60ae927a27
cayter <cayter@appist.io>
2021-04-23 11:12:34 +0800

    update cmd export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export type { Cmd } from "./cmd";
 export type { DB, DBConfig } from "./record";
 
-export * as cmd from "./cmd";
+export { default as cmd } from "./cmd";
 export * as record from "./record";
 export * as pack from "./pack";
 export * as support from "./support";
