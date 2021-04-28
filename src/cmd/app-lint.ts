d18866e672c8cd1e66fef89fad7a5e14e516adb5
cayter <cayter@appist.io>
2021-04-28 10:59:09 +0800

    add more stuffs

--- src/cmd/app-lint.ts
+++ src/cmd/app-lint.ts
@@ -7,10 +7,10 @@ cmd
   .action(async (opts) => {
     if (opts.fix) {
       execSync("npm exec prettier -- --write .", { stdio: "inherit" });
-      execSync("npm exec eslint -- --fix --ignore-path .gitignore .", { stdio: "inherit" });
+      execSync("npm exec eslint -- --fix .", { stdio: "inherit" });
       return;
     }
 
     execSync("npm exec prettier -- --check .", { stdio: "inherit" });
-    execSync("npm exec eslint -- --ignore-path .gitignore .", { stdio: "inherit" });
+    execSync("npm exec eslint -- .", { stdio: "inherit" });
   });
