088101758299ecbe5cd7ea833e1961ce96d8622e
cayter <cayter@appist.io>
2021-04-28 22:48:22 +0800

    add more stuffs

--- dist/cmd/app-lint.js
+++ dist/cmd/app-lint.js
@@ -3,10 +3,15 @@ import { default as cmd } from "./cmd";
 cmd
     .command("app:lint", "Lint/Format the code with ESLint and Prettier.")
     .option("--fix", "Fix the errors/warnings emitted by ESLint/Prettier.")
+    .option("--staged", "Run linters against staged git files.")
     .action(async (opts) => {
     if (opts.fix) {
         execSync("npm exec prettier -- --write .", { stdio: "inherit" });
         execSync("npm exec eslint -- --fix .", { stdio: "inherit" });
+        return;
+    }
+    if (opts.staged) {
+        execSync("npm exec lint-staged", { stdio: "inherit" });
         return;
     }
     execSync("npm exec prettier -- --check .", { stdio: "inherit" });
