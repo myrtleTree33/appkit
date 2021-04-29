8fee544304b34e9082d5e1d9c348df44d663df16
cayter <cayter@appist.io>
2021-04-29 16:07:16 +0800

    add more stuffs

--- src/cmd/build.ts
+++ src/cmd/build.ts
@@ -1,6 +1,14 @@
 import { execSync } from "child_process";
+import { readFileSync, rmSync } from "fs";
+import { resolve } from "path";
 import { default as cmd } from "./cmd";
 
 cmd.command("build", "Compile the Typescript source code into Javascript for production deployment.").action(() => {
+  const tsconfig = JSON.parse(readFileSync(`${process.cwd()}/tsconfig.json`, "utf-8"));
+
+  if (tsconfig.compilerOptions?.outDir) {
+    rmSync(resolve(`${process.cwd()}/${tsconfig.compilerOptions?.outDir}`), { recursive: true, force: true });
+  }
+
   execSync("npm exec tsc", { stdio: "inherit" });
 });
